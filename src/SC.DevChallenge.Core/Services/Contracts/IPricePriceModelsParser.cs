f312513772a7d5498507c062bdcc9af86312bb5f
misikdmitriy <misik.dmitriy@gmail.com>
2019-03-16 22:55:09 +0200

    remove unused

--- src/SC.DevChallenge.Core/Services/Contracts/IPricePriceModelsParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using SC.DevChallenge.Db.Models;
-
-namespace SC.DevChallenge.Core.Services.Contracts
-{
-    public interface IPriceModelParser
-    {
-        PriceModel ParseCsv(string line);
-    }
-}
